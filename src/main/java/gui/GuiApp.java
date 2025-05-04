843383700b7657f4ea3319ea16252c471477c678
Emir Hamulic <hamulicemir@icloud.com>
2025-05-04 19:25:06 +0200

    Änderungen der GUI

--- src/main/java/gui/GuiApp.java
+++ src/main/java/gui/GuiApp.java
@@ -1,20 +1,16 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class GuiApp extends Application {
-
     @Override
-    public void start(Stage primaryStage) {
-        Label label = new Label("Hello, Energy World!");
-        StackPane root = new StackPane(label);
-        Scene scene = new Scene(root, 400, 200);
-
-        primaryStage.setTitle("Energy Community GUI");
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/main.fxml"));
+        Scene scene = new Scene(loader.load());
+        primaryStage.setTitle("Energy Community Dashboard");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
