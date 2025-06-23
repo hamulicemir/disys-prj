3387251044c146f4b5ad58e11deeaba02cebb1ad
Adi Redzic <wi23b112@technikum-wien.at>
2025-06-23 21:02:45 +0200

    daten in die DB gespeichert

--- usage-service/src/main/java/at/fhtw/energy/config/MQConfig.java
+++ usage-service/src/main/java/at/fhtw/energy/config/MQConfig.java
@@ -1,40 +1,36 @@
 package at.fhtw.energy.config;
 
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+        import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+        import org.springframework.amqp.core.Queue;
+        import org.springframework.amqp.rabbit.core.RabbitTemplate;
+        import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+        import org.springframework.context.annotation.Bean;
+        import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class MQConfig {
-    public static final String ENERGY_QUEUE = "energy.queue";
-    public static final String UPDATE_QUEUE = "update.queue";
+        @Configuration
+        public class MQConfig {
+            public static final String ENERGY_QUEUE = "energy.queue";
+            public static final String UPDATE_QUEUE = "update.queue";
 
-    // Queue automatisch erstellen
-    @Bean
-    public Queue energyQueue() {
-        return new Queue(ENERGY_QUEUE, true); // durable = true
-    }
+            @Bean
+            public Queue energyQueue() {
+                return new Queue(ENERGY_QUEUE, true);
+            }
 
-    // JSON-Konverter für RabbitMQ
-    @Bean
-    public Jackson2JsonMessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
+            @Bean
+            public Queue updateQueue() {
+                return new Queue(UPDATE_QUEUE, true);
+            }
 
-    // Queue automatisch erstellen
-    @Bean
-    public Queue updateQueue() {
-        return new Queue(UPDATE_QUEUE, true);
-    }
+            @Bean
+            public Jackson2JsonMessageConverter jsonMessageConverter() {
+                return new Jackson2JsonMessageConverter();
+            }
 
-    // RabbitTemplate mit JSON-Konverter
-    @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        RabbitTemplate template = new RabbitTemplate(connectionFactory);
-        template.setMessageConverter(jsonMessageConverter());
-        return template;
-    }
-}
+            @Bean
+            public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, Jackson2JsonMessageConverter jsonMessageConverter) {
+                RabbitTemplate template = new RabbitTemplate(connectionFactory);
+                template.setMessageConverter(jsonMessageConverter);
+                return template;
+            }
+        }
