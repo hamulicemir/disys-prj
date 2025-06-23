c59c6a642fc3f5be9846bcb51f7aa0c84b6d9301
Mateja J <wi23b130@technikum-wien.at>
2025-06-24 00:07:51 +0200

    Alle Services laufen

--- current-percentage-service/src/main/java/at/fhtw/energy/CurrentPercentageService.java
+++ current-percentage-service/src/main/java/at/fhtw/energy/CurrentPercentageService.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableRabbit
+@EnableScheduling
 public class CurrentPercentageService
 {
     public static void main( String[] args )
--- energy-rest-api/src/main/java/at/fhtw/energy/EnergyRestApiApplication.java
+++ energy-rest-api/src/main/java/at/fhtw/energy/EnergyRestApiApplication.java
@@ -4,9 +4,11 @@ package at.fhtw.energy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "at.fhtw.energy.repository")
+@EnableScheduling
 public class EnergyRestApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(EnergyRestApiApplication.class, args);
